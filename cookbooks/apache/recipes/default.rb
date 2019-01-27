33633e5c477b79b7599132b571be5043e00087cb
Ed Mamerto <edmamerto@gmail.com>
2019-01-27 12:36:05 -0500

    add resources for apache

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+package 'httpd' do
+	action :install
+end
+
+file '/var/www/htmlindex.html' do
+	content 'Hello World!'
+end
+
+service 'httpd' do
+	action [:start, :enable]
+end
